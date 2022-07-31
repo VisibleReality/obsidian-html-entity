897c816fa5c84b8f59203319108565f9aca1179f
VisibleReality <37581230+VisibleReality@users.noreply.github.com>
2022-07-31 23:53:35 +1000

    Functional

--- main.ts
+++ main.ts
@@ -1,137 +1,87 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	MarkdownView,
+	Plugin
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import {
+	EditorState,
+	Transaction
+} from "@codemirror/state";
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const he = require("he");
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	prefixCharacter = "&"
+	suffixCharacter = ";"
 
 	async onload() {
-		await this.loadSettings();
+		// Register editorExtension to catch changes to the document
+		this.registerEditorExtension([
+			EditorState.transactionFilter.of(this.handleTransaction.bind(this))
+		]);
+	}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	handleTransaction(transaction: Transaction): Transaction {
+		// Ignore transactions which don't change the document
+		if (!transaction.isUserEvent("input.type") || !transaction.docChanged) {
+			return transaction;
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		let tryReplace = false;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		transaction.changes.iterChanges(
+			(fA, tA, fB, tB, inserted) => {
+				if (inserted.line(1).text[0] === this.suffixCharacter) {
+					tryReplace = true;
 				}
+			}, false)
+
+		if (tryReplace) {
+			this.decodeEntity();
+		}
+
+		return transaction;
+	}
+
+	decodeEntity() {
+		setTimeout(() => {
+			const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+
+			// If we couldn't get the active editor, return
+			if (!editor) {
+				return;
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			const cursor = editor.getCursor();
+			const text = editor.getLine(cursor.line);
+			const startIndex = text.lastIndexOf(this.prefixCharacter, cursor.ch);
+			const endIndex = text.indexOf(this.suffixCharacter, startIndex + 1);
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			// If we couldn't find the prefix or the suffix, or if the last character isn't part of the entity, exit out
+			if (startIndex === -1 || endIndex === -1 || endIndex + 1 < cursor.ch) {
+				return;
+			}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			const entityText = text.substring(startIndex, endIndex + 1);
+
+			const decodedText = he.decode(entityText);
+
+			// If the decoded text is the same, don't do anything
+			if (entityText === decodedText) {
+				return;
+			}
+
+			// Then, replace the text
+			editor.replaceRange(decodedText,
+				{line: cursor.line, ch: startIndex},
+				{line: cursor.line, ch: endIndex + 1}
+			);
+
+		}, 0)
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
